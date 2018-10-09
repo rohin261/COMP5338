c780763ff2968d22a113944566e81be3a4da7047
rohin261 <38878736+rohin261@users.noreply.github.com>
2018-10-09 16:56:21 +1100

    Add files via upload

--- AQ.js
+++ AQ.js
@@ -1,162 +1,128 @@
 //AQ1
-db.posts.aggregate([
-
-   {
-      "$lookup": {
-         "from": "posts",
-         "let": {
-            "answerId": "$AcceptedAnswerId"
-         },
-         "pipeline": [{
-            "$match": {
-               "$expr": {
-                  "$eq": ["$Id", "$$answerId"]
-               }
-            }
-         }],
-         "as": "dates"
-      }
-   },
-   {
-      $project: {
-         QuestionTitle: "$Title",
-         QuestionDate: "$CreationDate",
-         AnswerDate: {
-            $arrayElemAt: ["$dates.CreationDate", 0]
-         },
-         tag: {
-            $split: ["$Tags", ","]
-         }
-      }
-   },
-   {
-      $match: {
-         tag: "neural-networks" //user input
-      }
-   },
-   {
-      $project: {
-         Title: "$QuestionTitle",
-         TimeDifInHours: {
-            $divide: [{
-               $subtract: ["$AnswerDate", "$QuestionDate"]
-            }, (1000 * 60 * 60)] //convert time difference to Hours
-         }
-
-      }
-   },
-   {
-      $match: {
-         TimeDifInHours: {
-            "$exists": true,
-            "$ne": null
-         }
-      }
-   },
-   {
-      $sort: {
-         TimeDifInHours: 1 //Fastest time first
-      }
-   }, {
-      $limit: 1 
-   }
-
-])
-//AQ2
-
 db.posts.aggregate([{
-		$project: {
-			tag: {
-				$split: ["$Tags", ","]
-			},
-			CreationDate: "$CreationDate",
-			AnswerCount: "$AnswerCount",
-			Question: "$Id",
-			Owner: "$OwnerUserId"
-		}
-	},
-	{
-		$unwind: "$tag"
-	},
-
-	{
-		$lookup: {
-			"from": "posts",
-			"let": {
-				"yy": "$Question"
-			},
-			"pipeline": [{
-				"$match": {
-					"$expr": {
-						"$eq": ["$$yy", "$ParentId"]
-					}
-				},
-
-			}],
-			"as": "info"
-		}
-	},
-
-	{
-		$match: {
-			"CreationDate": {
-				$gte: ISODate("2018-08-01T00:00:00"),
-				$lte: ISODate("2018-08-31T00:00:00")
+	"$lookup": {
+		"from": "posts",
+		"let": {
+			"answerId": "$AcceptedAnswerId"
+		},
+		"pipeline": [{
+			"$match": {
+				"$expr": {
+					"$eq": ["$Id", "$$answerId"]
+				}
 			}
-		}
-	},
-
-
-	//{
-	// $match:{tag:"machine-learning"}
-	//},
-	//{$unwind:"$info"},
-
-	{
-		$group: {
-			_id: "$tag",
-			OOO: {
-				$addToSet: "$Owner"
-			},
-			Answerer: {
-				$addToSet: "$info.OwnerUserId"
-			}
-		}
-	},
-
-
-	{
-		$project: {
-			FinalTotal: {
-				"$add": [{
-					$size: "$OOO"
-				}, {
-					$size: "$Answerer"
-				}]
-			}
-		}
-	},
-
-	{
-		$project: {
-			Topic: "$tag",
-			TotalUsers: "$FinalTotal"
-		}
-	},
-	{
-		$sort: {
-			TotalUsers: -1
-		}
-	},
-	{
-		$limit: 5
+		}],
+		"as": "dates"
 	}
-
-])
-
+}, {
+	$project: {
+		QuestionTitle: "$Title",
+		QuestionDate: "$CreationDate",
+		AnswerDate: {
+			$arrayElemAt: ["$dates.CreationDate", 0]
+		},
+		tag: {
+			$split: ["$Tags", ","]
+		}
+	}
+}, {
+	$match: {
+		tag: "neural-networks" //user input
+	}
+}, {
+	$project: {
+		Title: "$QuestionTitle",
+		TimeDifInHours: {
+			$divide: [{
+				$subtract: ["$AnswerDate", "$QuestionDate"]
+			}, (1000 * 60 * 60)] //convert time difference to Hours
+		}
+	}
+}, {
+	$match: {
+		TimeDifInHours: {
+			"$exists": true,
+			"$ne": null
+		}
+	}
+}, {
+	$sort: {
+		TimeDifInHours: 1 //Fastest time first
+	}
+}, {
+	$limit: 1
+}])
+///AQ2
+db.posts.aggregate([{
+	$project: {
+		tag: {
+			$split: ["$Tags", ","]
+		},
+		CreationDate: "$CreationDate",
+		AnswerCount: "$AnswerCount",
+		Question: "$Id",
+		Owner: "$OwnerUserId"
+	}
+}, {
+	$unwind: "$tag"
+}, {
+	$lookup: {
+		"from": "posts",
+		"let": {
+			"yy": "$Question"
+		},
+		"pipeline": [{
+			"$match": {
+				"$expr": {
+					"$eq": ["$$yy", "$ParentId"]
+				}
+			},
+		}],
+		"as": "info"
+	}
+}, {
+	$match: {
+		"CreationDate": {
+			$gte: ISODate("2017-03-01T00:00:00"),
+			$lte: ISODate("2017-03-31T00:00:00")
+		}
+	}
+}, {
+	$unwind: "$info"
+}, {
+	$group: {
+		_id: "$tag",
+		OOO: {
+			$addToSet: "$Owner"
+		},
+		Answerer: {
+			$addToSet: "$info.OwnerUserId"
+		}
+	}
+}, {
+	$project: {
+		FinalTotal: {
+			"$add": [{
+				$size: "$OOO"
+			}, {
+				$size: "$Answerer"
+			}]
+		}
+	}
+}, {
+	$project: {
+		Topic: "$tag",
+		TotalUsers: "$FinalTotal"
+	}
+}, {
+	$sort: {
+		TotalUsers: -1
+	}
+}, {
+	$limit: 5
+}])
 
 //AQ3
-
 db.posts.aggregate([{
       "$lookup": {
          "from": "posts",
@@ -229,6 +195,4 @@ db.posts.aggregate([{
    {
       $limit: 1
    },
-
-
 ])
--- SQ.js
+++ SQ.js
@@ -1,72 +1,54 @@
 //SQ1
 db.posts.aggregate([{
-      $lookup: {
-         from: "users",
-         localField: "OwnerUserId",
-         foreignField: "Id",
-         as: "UserInfo"
-      }
-   },
-   {
-      $match: {
-         $or: [{
-               ParentId: 2277 //Changed from String to Int because of Pre-Processing function
-            },
-            {
-               Id: 2277
-            }
-         ]
-      }
-   },
-
-   {
-      $project: {
-         CreationDate: {
-            $arrayElemAt: ["$UserInfo.CreationDate", 0]
-         },
-         DisplayName: {
-            $arrayElemAt: ["$UserInfo.DisplayName", 0]
-         },
-         UpVotes: {
-            $arrayElemAt: ["$UserInfo.UpVotes", 0]
-         },
-         DownVotes: {
-            $arrayElemAt: ["$UserInfo.DownVotes", 0]
-         }
-      }
-   }
-
-
-])
+	$lookup: {
+		from: "users",
+		localField: "OwnerUserId",
+		foreignField: "Id",
+		as: "UserInfo"
+	}
+}, {
+	$match: {
+		$or: [{
+			ParentId: 2277 //Changed from String to Int because of Pre-Processing function
+		}, {
+			Id: 2277
+		}]
+	}
+}, {
+	$project: {
+		CreationDate: {
+			$arrayElemAt: ["$UserInfo.CreationDate", 0]
+		},
+		DisplayName: {
+			$arrayElemAt: ["$UserInfo.DisplayName", 0]
+		},
+		UpVotes: {
+			$arrayElemAt: ["$UserInfo.UpVotes", 0]
+		},
+		DownVotes: {
+			$arrayElemAt: ["$UserInfo.DownVotes", 0]
+		}
+	}
+}])
 
 //SQ2
-
-db.posts.aggregate([
-
-   {
-      $project: {
-         Tags: {
-            $split: ["$Tags", ","]
-         }, //added $split for tags
-         _id: "$Id",
-         Title: "$Title",
-         "views": "$ViewCount"
-      }
-   },
-   {
-      $match: {
-         Tags: "turing-test"//user-input
-      }
-   },
-   {
-      $sort: {
-         "views": -1
-      }
-   }, {
-      $limit: 1
-   }
-
-
-
-])
-
+db.posts.aggregate([{
+	$project: {
+		Tags: {
+			$split: ["$Tags", ","]
+		}, //added $split for tags
+		_id: "$Id",
+		Title: "$Title",
+		"views": "$ViewCount"
+	}
+}, {
+	$match: {
+		Tags: "turing-test" //user-input
+	}
+}, {
+	$sort: {
+		"views": -1
+	}
+}, {
+	$limit: 1
+}])
